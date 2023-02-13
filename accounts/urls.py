8c67d5cb058f7593b27f32d995b5b596301c80dd
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-02-14 03:04:22 +0600

    order update and delete added

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,9 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('products/', products, name='products'),
-    path('customer/<int:pk>/', customer, name='customer'),
+    path('customer/<str:pk>/', customer, name='customer'),
     path('create_order/', createOrder, name='create_order'),
+    path('update_order/<str:pk>/', updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/', deleteOrder, name='delete_order'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -38,6 +38,7 @@ def customer(request, pk):
 
 def createOrder(request):
     form = OrderForm()
+
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
@@ -46,3 +47,28 @@ def createOrder(request):
 
     context = {'form': form}
     return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context = {'order': order}
+    return render(request, 'accounts/delete.html', context)
+
