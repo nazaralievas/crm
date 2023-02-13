e0dd71aed7c818d83605999c5fb8bcb35fecfda4
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-02-14 02:36:59 +0600

    order creating form added

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('products/', products, name='products'),
     path('customer/<int:pk>/', customer, name='customer'),
+    path('create_order/', createOrder, name='create_order'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import OrderForm
 
 
 def home(request):
@@ -33,3 +34,15 @@ def customer(request, pk):
     orders = customer.order_set.all()
     context = {'customer': customer, 'orders': orders}
     return render(request, 'accounts/customer.html', context)
+
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
